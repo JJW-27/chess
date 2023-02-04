9d26d0fbd77cf5c0f63bf427bf37b95795913f40
JJW-27 <josephjonwilliams@outlook.com>
2023-02-04 21:48:25 +0000

    adds initial pieces

---
+++ src/components/squares/Piece.jsx
@@ -0,0 +1,76 @@
+import {
+  FaChessBishop,
+  FaChessKing,
+  FaChessKnight,
+  FaChessPawn,
+  FaChessQueen,
+  FaChessRook,
+} from 'react-icons/fa';
+
+const Piece = ({ pieceName, pieceColour }) => {
+  let piece;
+
+  if (pieceColour === 'black') {
+    switch (pieceName) {
+      case 'bishop':
+        piece = <FaChessBishop size={40} />;
+        break;
+
+      case 'king':
+        piece = <FaChessKing size={40} />;
+        break;
+
+      case 'knight':
+        piece = <FaChessKnight size={40} />;
+        break;
+
+      case 'pawn':
+        piece = <FaChessPawn size={40} />;
+        break;
+
+      case 'queen':
+        piece = <FaChessQueen size={40} />;
+        break;
+
+      case 'rook':
+        piece = <FaChessRook size={40} />;
+        break;
+
+      default:
+        break;
+    }
+  } else {
+    switch (pieceName) {
+      case 'bishop':
+        piece = <FaChessBishop size={40} color="white" />;
+        break;
+
+      case 'king':
+        piece = <FaChessKing size={40} color="white" />;
+        break;
+
+      case 'knight':
+        piece = <FaChessKnight size={40} color="white" />;
+        break;
+
+      case 'pawn':
+        piece = <FaChessPawn size={40} color="white" />;
+        break;
+
+      case 'queen':
+        piece = <FaChessQueen size={40} color="white" />;
+        break;
+
+      case 'rook':
+        piece = <FaChessRook size={40} color="white" />;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return piece;
+};
+
+export default Piece;
--- src/components/squares/Square.jsx
+++ src/components/squares/Square.jsx
@@ -1,3 +1,4 @@
+import Piece from './Piece';
 import { useState, useEffect } from 'react';
 
 const Square = ({
@@ -7,7 +8,56 @@ const Square = ({
   selectedSquare,
   setSelectedSquare,
 }) => {
-  const [squareData, setSquareData] = useState({ xCoord, yCoord });
+  const [squareData, setSquareData] = useState({
+    xCoord,
+    yCoord,
+  });
+
+  useEffect(() => {
+    let initialPieceColour;
+    if (squareData.yCoord >= 7) {
+      initialPieceColour = 'white';
+    } else if (squareData.yCoord <= 2) {
+      initialPieceColour = 'black';
+    }
+    let initialPieceName;
+    if (
+      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
+      (squareData.xCoord === 1 || squareData.xCoord === 8)
+    ) {
+      initialPieceName = 'rook';
+    } else if (
+      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
+      (squareData.xCoord === 2 || squareData.xCoord === 7)
+    ) {
+      initialPieceName = 'knight';
+    } else if (
+      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
+      (squareData.xCoord === 3 || squareData.xCoord === 6)
+    ) {
+      initialPieceName = 'bishop';
+    } else if (
+      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
+      squareData.xCoord === 4
+    ) {
+      initialPieceName = 'king';
+    } else if (
+      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
+      squareData.xCoord === 5
+    ) {
+      initialPieceName = 'queen';
+    } else if (squareData.yCoord === 2 || squareData.yCoord === 7) {
+      initialPieceName = 'pawn';
+    }
+
+    setSquareData(currData => {
+      return {
+        pieceName: initialPieceName,
+        pieceColour: initialPieceColour,
+        ...currData,
+      };
+    });
+  }, []);
 
   const squareColour = squareIsLight ? 'light-square' : 'dark-square';
 
@@ -20,6 +70,7 @@ const Square = ({
   const handleClick = e => {
     e.preventDefault();
     setSelectedSquare({ xCoord: squareData.xCoord, yCoord: squareData.yCoord });
+    console.log(squareData);
   };
 
   const handleClickOff = e => {
@@ -32,7 +83,12 @@ const Square = ({
       id={isSelected}
       onClick={handleClick}
       onBlur={handleClickOff}
-    ></button>
+    >
+      <Piece
+        pieceName={squareData.pieceName}
+        pieceColour={squareData.pieceColour}
+      />
+    </button>
   );
 };
 
