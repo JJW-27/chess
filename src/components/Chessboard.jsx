7a1e7fd7195f4eb5602ac140d0393533c561ec4f
JJW-27 <josephjonwilliams@outlook.com>
2023-02-08 19:42:58 +0000

    initial refactor

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -2,50 +2,73 @@ import Square from './squares/Square';
 import { useState, useEffect } from 'react';
 
 const Chessboard = () => {
-  const [boardState, setBoardState] = useState({});
+  const [boardState, setBoardState] = useState([]);
   const [selectedSquare, setSelectedSquare] = useState({});
   const [isPieceSelected, setIsPieceSelected] = useState(false);
   const [movePlayed, setMovePlayed] = useState(false);
   const [validSquares, setValidSquares] = useState([[]]);
 
-  // generates 64 squares with unique x and y coordinates, from top left to bottom right
-  useEffect(() => {
-    const initialBoardState = {}
-    const squareCoordinates = [];
-  let squareIsLight = true;
-  let count = 0;
-  for (let y = 8; y >= 1; y--) {
-    for (let x = 1; x <= 8; x++) {
-      squareCoordinates.push([x, y, squareIsLight, count]);
-      squareIsLight = !squareIsLight;
-      count++;
-      if (count % 8 === 0) {
+    useEffect(() => {
+    const initialBoardState = [];
+
+    let squareIsLight = true;
+    let count = 0;
+
+    for (let y = 8; y >= 1; y--) {
+      for (let x = 1; x <= 8; x++) {
+        initialBoardState.push({
+          x,
+          y,
+          pieceName: 'none',
+          pieceColour: 'none',
+          isSelected: 'false',
+          isValidSquare: 'false',
+          squareIsLight,
+        });
         squareIsLight = !squareIsLight;
+        count++;
+        if (count % 8 === 0) {
+          squareIsLight = !squareIsLight;
+        }
       }
     }
-  }
+    initialBoardState.forEach(square => {
+      if (square.y >= 7) {
+        square.pieceColour = 'black';
+      } else if (square.y <= 2) {
+        square.pieceColour = 'white';
+      }
+
+      if (
+        (square.y === 1 || square.y === 8) &&
+        (square.x === 1 || square.x === 8)
+      ) {
+        square.pieceName = 'rook';
+      } else if (
+        (square.y === 1 || square.y === 8) &&
+        (square.x === 2 || square.x === 7)
+      ) {
+        square.pieceName = 'knight';
+      } else if (
+        (square.y === 1 || square.y === 8) &&
+        (square.x === 3 || square.x === 6)
+      ) {
+        square.pieceName = 'bishop';
+      } else if ((square.y === 1 || square.y === 8) && square.x === 4) {
+        square.pieceName = 'queen';
+      } else if ((square.y === 1 || square.y === 8) && square.x === 5) {
+        square.pieceName = 'king';
+      } else if (square.y === 2 || square.y === 7) {
+        square.pieceName = 'pawn';
+      }
+    });
+    setBoardState(initialBoardState);
   }, []);
-  
 
   return (
     <div className="chessboard">
-      {squareCoordinates.map(coord => {
-        return (
-          <Square
-            squareIsLight={coord[2]}
-            xCoord={coord[0]}
-            yCoord={coord[1]}
-            key={coord[3]}
-            selectedSquare={selectedSquare}
-            setSelectedSquare={setSelectedSquare}
-            isPieceSelected={isPieceSelected}
-            setIsPieceSelected={setIsPieceSelected}
-            movePlayed={movePlayed}
-            setMovePlayed={setMovePlayed}
-            validSquares={validSquares}
-            setValidSquares={setValidSquares}
-          />
-        );
+      {boardState.map(square => {
+        return <Square square={square} />;
       })}
     </div>
   );
--- src/components/squares/Square.jsx
+++ src/components/squares/Square.jsx
@@ -2,157 +2,102 @@ import Piece from './Piece';
 import { useState, useEffect } from 'react';
 import checkValidSquares from '../../utils/checkValidSquares';
 
-const Square = ({
-  xCoord,
-  yCoord,
-  squareIsLight,
-  selectedSquare,
-  setSelectedSquare,
-  isPieceSelected,
-  setIsPieceSelected,
-  movePlayed,
-  setMovePlayed,
-  validSquares,
-  setValidSquares,
-}) => {
-  const [squareData, setSquareData] = useState({
-    xCoord,
-    yCoord,
-  });
+const Square = ({ square }) => {
+  const {
+    x,
+    y,
+    pieceName,
+    pieceColour,
+    isSelected,
+    isValidSquare,
+    squareIsLight,
+  } = square;
 
-  // sets initial boardstate
-  useEffect(() => {
-    let initialPieceColour;
-    if (squareData.yCoord >= 7) {
-      initialPieceColour = 'black';
-    } else if (squareData.yCoord <= 2) {
-      initialPieceColour = 'white';
-    }
-    let initialPieceName;
-    if (
-      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
-      (squareData.xCoord === 1 || squareData.xCoord === 8)
-    ) {
-      initialPieceName = 'rook';
-    } else if (
-      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
-      (squareData.xCoord === 2 || squareData.xCoord === 7)
-    ) {
-      initialPieceName = 'knight';
-    } else if (
-      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
-      (squareData.xCoord === 3 || squareData.xCoord === 6)
-    ) {
-      initialPieceName = 'bishop';
-    } else if (
-      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
-      squareData.xCoord === 4
-    ) {
-      initialPieceName = 'queen';
-    } else if (
-      (squareData.yCoord === 1 || squareData.yCoord === 8) &&
-      squareData.xCoord === 5
-    ) {
-      initialPieceName = 'king';
-    } else if (squareData.yCoord === 2 || squareData.yCoord === 7) {
-      initialPieceName = 'pawn';
-    }
-
-    setSquareData(currData => {
-      return {
-        pieceName: initialPieceName,
-        pieceColour: initialPieceColour,
-        ...currData,
-      };
-    });
-  }, []);
-
-  useEffect(() => {
-    if (
-      selectedSquare.xCoord === squareData.xCoord &&
-      selectedSquare.yCoord === squareData.yCoord
-    ) {
-      setSquareData(currData => {
-        const newData = { ...currData };
-        delete newData.pieceName;
-        delete newData.pieceColour;
-        return newData;
-      });
-      setSelectedSquare({});
-      setIsPieceSelected(false);
-    }
-  }, [movePlayed]);
-
-  // sets CSS class dynamically
   const squareColour = squareIsLight ? 'light-square' : 'dark-square';
 
-  // sets CSS id dynamically
-  let selectedOrValid;
+  // useEffect(() => {
+  //   if (
+  //     selectedSquare.xCoord === squareData.xCoord &&
+  //     selectedSquare.yCoord === squareData.yCoord
+  //   ) {
+  //     setSquareData(currData => {
+  //       const newData = { ...currData };
+  //       delete newData.pieceName;
+  //       delete newData.pieceColour;
+  //       return newData;
+  //     });
+  //     setSelectedSquare({});
+  //     setIsPieceSelected(false);
+  //   }
+  // }, [movePlayed]);
 
-  if (
-    selectedSquare.xCoord === squareData.xCoord &&
-    selectedSquare.yCoord === squareData.yCoord
-  ) {
-    selectedOrValid = 'selected';
-    
-  }
+  // // sets CSS class dynamically
+  // const squareColour = squareIsLight ? 'light-square' : 'dark-square';
 
-  const handleClick = e => {
-    e.preventDefault();
-    // if the clicked square has a piece and a piece isn't already selected
-    if (squareData.pieceName && !isPieceSelected) {
-      setSelectedSquare({
-        xCoord: squareData.xCoord,
-        yCoord: squareData.yCoord,
-        pieceName: squareData.pieceName,
-        pieceColour: squareData.pieceColour,
-      });
-      setIsPieceSelected(true);
-    }
-    // if a piece is selected, only allow selecting of same colour piece
-    else if (squareData.pieceColour === selectedSquare.pieceColour) {
-      setSelectedSquare({
-        xCoord: squareData.xCoord,
-        yCoord: squareData.yCoord,
-        pieceName: squareData.pieceName,
-        pieceColour: squareData.pieceColour,
-      });
-    }
-    // if a piece is selected and the target square has no piece, or a piece of the opposite colour, add that piece to the target square and clear the selected square
-    else if (
-      isPieceSelected &&
-      squareData.pieceColour !== selectedSquare.pieceColour
-    ) {
-      setSquareData(currData => {
-        return {
-          ...currData,
-          pieceName: selectedSquare.pieceName,
-          pieceColour: selectedSquare.pieceColour,
-        };
-      });
-      setMovePlayed(played => {
-        return !played;
-      });
-    }
-  };
+  // // sets CSS id dynamically
+  // let selectedOrValid;
 
-  useEffect(() => {
-    if (isPieceSelected) {
-      const returnedValidSquares = checkValidSquares(squareData);
-      setValidSquares(returnedValidSquares);
-      if (validSquares.includes([squareData.xCoord, squareData.yCoord])) {
-        selectedOrValid = 'valid';
-        console.log('true');
-      }
-    }
-  }, [isPieceSelected]);
+  // if (
+  //   selectedSquare.xCoord === squareData.xCoord &&
+  //   selectedSquare.yCoord === squareData.yCoord
+  // ) {
+  //   selectedOrValid = 'selected';
+
+  // }
+
+  // const handleClick = e => {
+  //   e.preventDefault();
+  //   // if the clicked square has a piece and a piece isn't already selected
+  //   if (squareData.pieceName && !isPieceSelected) {
+  //     setSelectedSquare({
+  //       xCoord: squareData.xCoord,
+  //       yCoord: squareData.yCoord,
+  //       pieceName: squareData.pieceName,
+  //       pieceColour: squareData.pieceColour,
+  //     });
+  //     setIsPieceSelected(true);
+  //   }
+  //   // if a piece is selected, only allow selecting of same colour piece
+  //   else if (squareData.pieceColour === selectedSquare.pieceColour) {
+  //     setSelectedSquare({
+  //       xCoord: squareData.xCoord,
+  //       yCoord: squareData.yCoord,
+  //       pieceName: squareData.pieceName,
+  //       pieceColour: squareData.pieceColour,
+  //     });
+  //   }
+  //   // if a piece is selected and the target square has no piece, or a piece of the opposite colour, add that piece to the target square and clear the selected square
+  //   else if (
+  //     isPieceSelected &&
+  //     squareData.pieceColour !== selectedSquare.pieceColour
+  //   ) {
+  //     setSquareData(currData => {
+  //       return {
+  //         ...currData,
+  //         pieceName: selectedSquare.pieceName,
+  //         pieceColour: selectedSquare.pieceColour,
+  //       };
+  //     });
+  //     setMovePlayed(played => {
+  //       return !played;
+  //     });
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (isPieceSelected) {
+  //     const returnedValidSquares = checkValidSquares(squareData);
+  //     setValidSquares(returnedValidSquares);
+  //     if (validSquares.includes([squareData.xCoord, squareData.yCoord])) {
+  //       selectedOrValid = 'valid';
+  //       console.log('true');
+  //     }
+  //   }
+  // }, [isPieceSelected]);
 
   return (
-    <button className={squareColour} id={selectedOrValid} onClick={handleClick}>
-      <Piece
-        pieceName={squareData.pieceName}
-        pieceColour={squareData.pieceColour}
-      />
+    <button className={squareColour}>
+      <Piece pieceName={pieceName} pieceColour={pieceColour} />
     </button>
   );
 };
